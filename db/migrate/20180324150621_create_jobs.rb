bad9451f535c819bc7908447e0de352b790192d4
christophe <chris.l.honore@gmail.com>
2018-03-24 16:12:58 +0100

    view version

---
+++ db/migrate/20180324150621_create_jobs.rb
@@ -0,0 +1,14 @@
+class CreateJobs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :company
+      t.string :desc
+      t.string :url
+      t.string :contact
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
