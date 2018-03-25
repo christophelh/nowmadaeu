842a0ef5ebe5b7ee618d86c303fcee87060ffa7b
christophe <chris.l.honore@gmail.com>
2018-03-25 17:20:19 +0200

    meta tag + devise remove

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   # after_action :verify_authorized, except: :index, unless: :skip_pundit?
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-
+def default_url_options
+  { host: ENV['HOST'] || 'localhost:3000' }
+end
 
   #   def skip_pundit?
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
