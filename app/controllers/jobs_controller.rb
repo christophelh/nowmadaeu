418462923bf5d858c7b01715d18cc043eec2cb63
christophe <chris.l.honore@gmail.com>
2018-03-30 01:52:13 +0200

    date modified again

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,7 +7,7 @@ class JobsController < ApplicationController
 
   def index
      @weekjobs = Job.where('created_at <= ? and created_at > ?', Date.today, Date.today - 6 ).order("created_at DESC")
-      @alljobs = Job.where('created_at >= ?', Date.today - 7).order("created_at DESC")
+      @alljobs = Job.where('created_at <= ?', Date.today - 7).order("created_at DESC")
     @user = current_user
     @meta_title = meta_title 'Portal de empleo remoto y recursos en español'
   end
