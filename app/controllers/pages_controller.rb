8711010fd0214d3ea88e3a1ce09b0bddd7d9c0f2
christophe <chris.l.honore@gmail.com>
2018-04-19 09:30:51 +0200

    finalised workshop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[trabajo]
+  skip_before_action :authenticate_user!, only: %i[trabajo workshop]
 
   def workshop
   end
